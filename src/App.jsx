0ea17b66262356a88c8c0b29513769bc8cfbf907
Fiqri Ramadhan Jupri <fiqrirj13@gmail.com>
2023-07-12 23:15:50 +0700

    fix router

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,36 @@ import Certificate from "./pages/Certificate";
 import Contact from "./pages/Contact";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/about",
-      element: <About />,
-    },
-    {
-      path: "/skill",
-      element: <Skill />,
-    },
-    {
-      path: "/portfolio",
-      element: <Portfolio />,
-    },
-    {
-      path: "/certificate",
-      element: <Certificate />,
-    },
-    {
-      path: "/contact",
-      element: <Contact />,
-    },
-  ]);
+  const router = createBrowserRouter(
+    routes,
+    { basename: import.meta.env.DEV ? "/" : "/vite-porto/" },
+    [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/skill",
+        element: <Skill />,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio />,
+      },
+      {
+        path: "/certificate",
+        element: <Certificate />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ]
+  );
 
   return <RouterProvider router={router} />;
 }
